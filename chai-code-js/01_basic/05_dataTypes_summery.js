4fc948e2cec3129095442c14c44d49f0d278ac04
Subhadip023 <subhadip240420@gmail.com>
2024-07-02 19:43:12 +0530

    datatypes interview and thery

---
+++ chai-code-js/01_basic/05_dataTypes_summery.js
@@ -0,0 +1,30 @@
+/*
+Data types are 2 types based on data set on memory store and access (call by value or call by reference )
+
+1.Primitive datatype
+    String ,Number ,Boolean ,null ,undefined ,Symbol, BigInt 
+
+
+2.Non Primitive (reference)    
+    Array,Objects,Function 
+
+js is dynamically Typed     
+*/
+
+// const score = 100 
+
+const id =Symbol('123')
+const anotherId=Symbol('123')
+
+const bigNumber=4356453543645364354634637n
+// console.log(typeof bigNumber)
+
+// console.log(id,anotherId)
+// console.log(id==anotherId)
+
+const heros=["shaktiman","naagraj","doga"]
+
+let myObj={
+    name:"subhadip",
+    age:24,
+}
