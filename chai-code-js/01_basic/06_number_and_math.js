966901ea2b18715e0c50726d3f334454fda96a66
Subhadip023 <subhadip240420@gmail.com>
2024-07-04 11:34:13 +0530

    number and math

---
+++ chai-code-js/01_basic/06_number_and_math.js
@@ -0,0 +1,37 @@
+const score=300
+const balence =new Number (800)
+
+// console.log(score)
+// console.log(typeof balence.toString())
+// console.log( balence.toFixed(2))
+
+const otherNumber =123.989454
+
+// console.log(otherNumber.toPrecision(5))
+
+const hundreds =10000000
+// console.log(hundreds.toLocaleString('en-us'))
+
+// ++++++++++++++++++++++++Maths++++++++++++++++++++++++
+
+// console.log(Math)
+// console.log(Math.abs(-4))
+// console.log(Math.round(4.3))
+// console.log(Math.ceil(4.3))
+// console.log(Math.floor(4.3))
+// console.log(Math.min(4,5,6,9,20,10,2,1,40,7,8))
+// console.log(Math.max(4,5,6,9,20,10,2,1,40,7,8))
+// console.log(Math.random());
+// console.log(Math.floor(6*(Math.random()))+1);
+
+// get a Random number in range of min and maximum value 
+
+const min=10
+const max=20
+
+// console.log(Math.floor(Math.random()*(max-min+1) +min))
+
+for( let i=0;i<=100;i++){
+    console.log(Math.floor(Math.random()*(max-min+1) +min))
+}
+
