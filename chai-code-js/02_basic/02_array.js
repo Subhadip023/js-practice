671db39ffceb133a89b7966c228bd75bc25794a3
Subhadip023 <subhadip240420@gmail.com>
2024-07-07 13:24:53 +0530

    array 2.0

---
+++ chai-code-js/02_basic/02_array.js
@@ -0,0 +1,26 @@
+const marvel_heros=['Thor','Iron Man','Spiderman'];
+const dc_heros=['superman','flash','batman'];
+
+// marvel_heros.push(dc_heros)
+
+// const allHeros=marvel_heros.concat(dc_heros);//retuen new array 
+
+// console.log(allHeros);
+
+// const allHeros=[...mar
+
+
+// const myArray=[1,2,3,[4,5,6],7,[6,7,[4,5]]]
+
+// const usealeMyArray=myArray.flat(Infinity)
+
+// console.log(usealeMyArray)
+
+
+console.log(Array.isArray("subhadip"))
+console.log(Array.from("subhadip"))
+console.log(Array.from({name:"subhadip"}))// interesting this retune '[]' as for object must decleard the key or values 
+let score1=100;
+let score2=200;
+let score3=300;
+console.log(Array.of(score1,score2 ,score3))
